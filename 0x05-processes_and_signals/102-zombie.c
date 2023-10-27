1ed480dede94a31ee651532819e408d6d2bf24c4
Thejo96 <thejopodcast2023@gmail.com>
2023-10-27 09:28:25 +0100

    Updated

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,27 +1,11 @@
-/*
- * File_Name: 102-zombie.c
- * Created: 28th June, 2023
- * Author: David James Taiye (Official0mega)
- * Size_Of_File: Undefined
- * Project_Title: sorting_algorithms
- * Status: Submitted.!
- */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 
 /**
- * infinite_while - Initializes an infinite while loop.
- *
- * Return: Always returns 0.
- *
- * Description:
- * This function creates an infinite loop using a while statement.It repeatedly
- * calls the sleep() function to pause the execution for 1 second, allowing the
- * program to continue running indefinitely until interrupted by a signal. The
- * function returns 0 to indicate successful execution.
- */
+ * infinite_while - a function that runs forever and returns nothing
+ * Return: 0 in the end
+*/
 int infinite_while(void)
 {
 	while (1)
@@ -32,54 +16,23 @@ int infinite_while(void)
 }
 
 /**
- * create_process - Creates a new process and prints the PID of the new process
- *
- * Description:
- * This function creates a new child process using the fork() system call. The
- * fork() system call creates a new process by duplicating the calling process.
- * The child process starts execution from the point where fork() was called.
- * The parent process continues execution after creating the child process. In
- * the child process, the function prints its own process ID (PID) to the
- * standard output using the getpid() function. The parent process does not
- * print anything and continues its execution.
- */
-void create_process(void)
-{
-	int rc = fork();
-
-	if (rc == 0)
-	{
-		/* Child process */
-		printf("Zombie process created, PID: %d\n", getpid());
-		exit(0);
-	}
-}
-
-/**
- * main - Creates 5 zombie processes.
- *
- * Return: Always returns 0.
- *
- * Description:
- * This is the main entry point of the program.It creates 5 zombie processes by
- * calling the create_process() function five times. Each process is created as
- * a child of the main process using the fork() system call. The fork() system
- * call creates a new child process by duplicating the main process. The child
- * processes created by calling create_process() print their own process IDs
- * (PIDs) to the standard output. The main process, which is the parent, does
- * not print anything. After creating the child processes, the main process
- * enters an infinite while loop by calling the infinite_while() function. The
- * infinite_while() function keep the program running indefinitely by executing
- * an infinite loop that repeatedly calls the sleep() function to pause the
- * execution for 1 second.
- */
+ * main - the entry to a program that creats 5 zombie process
+ * Return: 0 on sucess
+*/
 int main(void)
 {
-	create_process();
-	create_process();
-	create_process();
-	create_process();
-	create_process();
-	return (infinite_while());
-}
+	int children_processes = 0;
+	pid_t pid;
 
+	while (children_processes < 5)
+	{
+		pid = fork();
+		if (!pid)
+			break;
+		printf("Zombie process created, PID: %i\n", (int)pid);
+		children_processes++;
+	}
+	if (pid != 0)
+		infinite_while();
+	return (0);
+}
